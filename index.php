acae01202f6635f6e4888fe4f2016d17f3e93196
Hrant Abelyan <electronemail777@gmail.com>
2018-04-21 23:17:33 +0400

    created basic UI :)

--- index.php
+++ index.php
@@ -2,3 +2,18 @@
   require 'config/config.php';
 ?>
 
+<h1>Boogle</h1>
+
+<h2>Submit to index</h2>
+<form action=''>
+  <input name='url' type='text' />
+  <input name='submit-site' type='submit' value='Index it' />
+</form>
+
+<hr />
+
+<h2>Search</h2>
+<form action=''>
+  <input name='search-query' type='text' />
+  <input name='submit-search' type='submit' value='Search' />
+</form>
